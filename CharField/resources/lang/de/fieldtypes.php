031d3eccb195a0ba4c7385c97bbb4cb63dbfc74e
Rémy Böhler <remy.boehler@subpixel.ch>
2016-11-25 16:15:19 +0100

    german translation

---
+++ CharField/resources/lang/de/fieldtypes.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+    // field type options
+    'input_type' => 'Eingabetyp',
+    'low' => 'Zeichen minimal',
+    'high' => 'Zeichen maximal',
+    'optimal_min' => 'Zeichen minimal (optimal)',
+    'optimal_max' => 'Zeichen maximal (optimal)',
+    'hard_limit' => 'Maximale Anzahl Zeichen die eingegeben werden kann',
+
+    // used in vue component
+    'current' => 'Aktuell',
+    'ideal_x' => 'Ideal :min bis :max Zeichen',
+    'min_x' => 'min. :min Zeichen',
+    'max_x' => 'max. :max Zeichen',
+];
