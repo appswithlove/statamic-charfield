985883fe489384d5be546163e59043b0f2829013
carratillon <carra.tillon@gmail.com>
2023-02-21 13:44:40 +0100

    Translate statamic charfield addon settings to french

---
+++ resources/lang/fr/fieldtypes.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+    // field type options
+    'input_type' => "Type d'entrée",
+    'low' => 'Min nombre de chars',
+    'high' => 'Max nombre de chars',
+    'optimal_min' => 'Min nombre de chars optimal',
+    'optimal_max' => 'Max nombre de chars optimal',
+    'hard_limit' => 'Limite dure',
+
+    // used in vue component
+    'current' => 'Actuel',
+    'ideal_x' => "Idéalement de :min jusqu'à :max chars",
+    'min_x' => 'min. :min chars',
+    'max_x' => 'max. :max chars',
+];
